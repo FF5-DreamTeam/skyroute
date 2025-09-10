de9a2008bfa3df80d4ea939ae5d29f364314ad13
vitaFlash <vitaliaflash@gmail.com>
2025-09-10 14:11:38 +0200

    first commit

---
+++ src/test/java/com/skyroute/skyroute/SkyrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyroute.skyroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
