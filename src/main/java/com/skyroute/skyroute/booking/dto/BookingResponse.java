49008ac61ffe737526c239ba8c675e6b7e6156e8
J-uds <206530139+J-uds@users.noreply.github.com>
2025-09-11 17:13:37 +0200

    feat: create BookingResponse dto

---
+++ src/main/java/com/skyroute/skyroute/booking/dto/BookingResponse.java
@@ -0,0 +1,21 @@
+package com.skyroute.skyroute.booking.dto;
+
+import java.time.LocalDateTime;
+
+public record BookingResponse(
+        Long bookingId,
+        String bookingNumber,
+        String bookingStatus,
+        Long flightId,
+        String flightNumber,
+        String origin,
+        String destination,
+        LocalDateTime departureTime,
+        LocalDateTime arrivalTime,
+        String passengerName,
+        String passengerPassport,
+        int seatBooked,
+        Double price,
+        LocalDateTime createdAt
+) {
+}
