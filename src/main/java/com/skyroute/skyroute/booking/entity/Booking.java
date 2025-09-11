0e0c73ba9595ff546f0397e14dc85dfcc82c2035
J-uds <206530139+J-uds@users.noreply.github.com>
2025-09-11 14:40:31 +0200

    feat: create booking entity with its attributes

---
+++ src/main/java/com/skyroute/skyroute/booking/entity/Booking.java
@@ -0,0 +1,37 @@
+package com.skyroute.skyroute.booking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "booking")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking /*extends auditable*/ {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "booking_number", nullable = false, length = 50)
+    private String bookingNumber;
+
+    @Column(name= "seats_booked")
+    private int seatsBooked;
+
+    @Column(name= "total_price", nullable = false)
+    private Double totalPrice;
+
+    /*@Column(name= "booking_status", nullable = false)
+    private BookingStatus bookingStatus;*/
+
+    /*@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;*/
+
+    /*@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "flight_id", referencedColumnName = "id", nullable = false)
+    private Flight flight;*/
+}
