c090f3842272f8d3cead8f8b3c900540444c45de
J-uds <206530139+J-uds@users.noreply.github.com>
2025-09-15 15:15:06 +0200

    refactor: BookingResponse to add updatedAt and change id for bookingId

--- src/main/java/com/skyroute/skyroute/booking/dto/BookingResponse.java
+++ src/main/java/com/skyroute/skyroute/booking/dto/BookingResponse.java
@@ -1,22 +1,24 @@
 package com.skyroute.skyroute.booking.dto;
 
+import com.skyroute.skyroute.booking.enums.BookingStatus;
+
 import java.time.LocalDateTime;
 
 public record BookingResponse(
         Long bookingId,
         String bookingNumber,
-        /*BookingStatus bookingStatus,*/
+        BookingStatus bookingStatus,
         Long flightId,
         String flightNumber,
         String origin,
         String destination,
         LocalDateTime departureTime,
         LocalDateTime arrivalTime,
-        String passengerName,
-        String passengerPassport,
-        String passengerBirthDate,
-        int seatBooked,
-        Double price,
-        LocalDateTime createdAt
+        String passengerNames,
+        String passengerBirthDates,
+        int seatsBooked,
+        Double totalPrice,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
 ) {
 }
