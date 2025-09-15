b6c649c34d1c08eb30debb0878ef6c4c8b308de6
J-uds <206530139+J-uds@users.noreply.github.com>
2025-09-15 16:04:15 +0200

    refactor: Booking to addapt to lists

--- src/main/java/com/skyroute/skyroute/booking/entity/Booking.java
+++ src/main/java/com/skyroute/skyroute/booking/entity/Booking.java
@@ -1,7 +1,7 @@
 package com.skyroute.skyroute.booking.entity;
 
 import com.skyroute.skyroute.booking.enums.BookingStatus;
-import com.skyroute.skyroute.shared.Auditable;
+import com.skyroute.skyroute.shared.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,31 +17,33 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Booking extends Auditable {
+public class Booking extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name= "booking_number", nullable = false, length = 50)
+    @Column(name= "booking_number", nullable = false, unique = true, length = 50)
     private String bookingNumber;
 
     @Column(name= "seats_booked")
     private int seatsBooked;
 
+    @ElementCollection
+    @CollectionTable(name = "booking_passenger_names", joinColumns = @JoinColumn(name = "booking_id"))
     @Column(name= "passenger_names")
     private List<String> passengerNames;
 
-    @Column(name= "passenger_passports")
-    private List<String> passengerPassports;
-
+    @ElementCollection
+    @CollectionTable(name = "booking_passenger_birth_dates", joinColumns = @JoinColumn(name = "booking_id"))
     @Column(name= "passenger_birth_dates")
     private List<LocalDate> passengerBirthDates;
 
-    @Column(nullable = false)
-    private Double price;
+    @Column(nullable = false, precision = 8, scale = 2)
+    private Double totalPrice;
 
     @Enumerated(EnumType.STRING)
-    @Column(name= "booking_status")
+    @Column(name= "booking_status", nullable = false)
     private BookingStatus bookingStatus;
 
     /*@ManyToOne(fetch = FetchType.LAZY)
