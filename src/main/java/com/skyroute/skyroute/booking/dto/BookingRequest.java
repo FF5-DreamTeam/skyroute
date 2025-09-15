163b7f438a35f054afee23431a345f01130620ce
J-uds <206530139+J-uds@users.noreply.github.com>
2025-09-15 15:07:59 +0200

    refactor: dto BookingRequest with more validations and lists for passenger names and birthdates, and add dto validation method

--- src/main/java/com/skyroute/skyroute/booking/dto/BookingRequest.java
+++ src/main/java/com/skyroute/skyroute/booking/dto/BookingRequest.java
@@ -1,27 +1,36 @@
 package com.skyroute.skyroute.booking.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record BookingRequest(
-    @NotNull
+    @NotNull(message = "Flight ID is required")
+    @Positive(message = "Flight ID must be positive")
     Long flightId,
 
-    @NotBlank(message = "Full name of passenger needed")
-    List<String> passengerNames,
+    @Positive(message = "Flight ID must be positive")
+    @Max(value = 10, message = "Maximum 10 seats per booking")
+    int seatsBooked,
 
-    @NotBlank(message = "Passport of passenger needed")
-    @Pattern(regexp = "^[A-Z0-9]+$", message = "Passport must contain capital letters and numbers only")
-    List<String> passengerPassports,
+    @NotEmpty(message = "At least one passenger name is required")
+    @Size(min = 1, max = 10, message = "Between 1 and 10 passenger allowed")
+    List<@NotBlank(message = "Passenger name required") @Size(min = 2, max = 100, message = "Passenger name must contain between 2 and 100 characters") String> passengerNames,
 
-    @NotNull(message = "Birth date of passenger needed")
-    @Past(message = "Birth date must be in the past")
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    List<LocalDate> passengerBirthDates,
-    int seatsBooked/*,
-    User user*/
+
+    @NotEmpty(message = "At least one birth date is required")
+    @Size(min = 1, max = 10, message = "birth dates must match the number of passengers")
+    List<@NotNull(message = "Birth date cannot be null") @Past(message = "Birth date must be in the past") LocalDate> passengerBirthDates
 ) {
+    public BookingRequest {
+        if (passengerNames != null && passengerBirthDates != null) {
+            if (passengerNames.size() != passengerBirthDates.size()) {
+                throw  new IllegalArgumentException("Number of passenger names must match number of passenger birth dates");
+            }
+            if (seatsBooked != passengerNames.size()) {
+                throw new IllegalArgumentException("Number of seats must match number of passengers");
+            }
+        }
+    }
 }
