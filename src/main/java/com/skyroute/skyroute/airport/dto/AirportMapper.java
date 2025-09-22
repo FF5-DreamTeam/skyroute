cf2d466590a1d39d4bc667956de33bab5aad0ebf
Alexandracoder <acasaviajes@gmail.com>
2025-09-22 10:26:29 +0200

    fix: add class AirportMapper to my branch

--- src/main/java/com/skyroute/skyroute/airport/dto/AirportMapper.java
+++ src/main/java/com/skyroute/skyroute/airport/dto/AirportMapper.java
@@ -3,7 +3,7 @@ package com.skyroute.skyroute.airport.dto;
 import com.skyroute.skyroute.airport.entity.Airport;
 
 public class AirportMapper {
-    public static Airport toEntity(AirportRequest dto){
+    public static Airport toEntity(AirportRequest dto) {
         return Airport.builder()
                 .code(dto.code())
                 .city(dto.city())
@@ -11,12 +11,20 @@ public class AirportMapper {
                 .build();
     }
 
-    public static AirportResponse toDto(Airport airport){
+    public static AirportResponse toDto(Airport airport) {
         return new AirportResponse(
                 airport.getId(),
                 airport.getCode(),
                 airport.getCity(),
                 airport.getImageUrl()
         );
+    }
+
+    public static Airport toEntityFromCreate(AirportCreateRequest dto, String imageUrl) {
+        return Airport.builder()
+                .code(dto.code())
+                .city(dto.city())
+                .imageUrl(imageUrl)
+                .build();
     }
 }
