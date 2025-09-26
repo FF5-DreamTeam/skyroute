96b435dcaa1348762595d24e1f5744c04f2433d2
Alexandracoder <acasaviajes@gmail.com>
2025-09-26 19:26:55 +0200

    feat: add configuration to public controller and pagination

--- src/main/java/com/skyroute/skyroute/flight/service/publicapi/FlightPublicService.java
+++ src/main/java/com/skyroute/skyroute/flight/service/publicapi/FlightPublicService.java
@@ -3,15 +3,15 @@ package com.skyroute.skyroute.flight.service.publicapi;
 import com.skyroute.skyroute.flight.dto.publicapi.FlightSearchRequest;
 import com.skyroute.skyroute.flight.dto.publicapi.FlightSimpleResponse;
 import com.skyroute.skyroute.flight.entity.Flight;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface FlightPublicService {
-
-    List<FlightSimpleResponse> searchFlights(FlightSearchRequest request);
 
     FlightSimpleResponse getFlightById(Long id);
 
     Flight findById(Long id);
+
+    Page<FlightSimpleResponse> searchFlights(FlightSearchRequest request, Pageable pageable);
 }
 
