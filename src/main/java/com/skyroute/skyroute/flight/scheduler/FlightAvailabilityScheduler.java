f5a728ab4029460da86bb5c33e91ab20f76c8238
Alexandracoder <acasaviajes@gmail.com>
2025-09-30 22:12:19 +0200

    feat: add scheduler to Flight entity

---
+++ src/main/java/com/skyroute/skyroute/flight/scheduler/FlightAvailabilityScheduler.java
@@ -0,0 +1,24 @@
+package com.skyroute.skyroute.flight.scheduler;
+
+import com.skyroute.skyroute.flight.service.FlightService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class FlightAvailabilityScheduler {
+
+    private final FlightService flightService;
+
+    @Scheduled(cron = "${app.scheduler.flight-availability-cron}")
+    public void updateFlightsAvailability() {
+        LocalDateTime now = LocalDateTime.now();
+        int updatedFlights = flightService.markFlightsAsUnavailableAndReleaseSeats(now);
+        log.info("Scheduler executed at {} - updated flights: {}", now, updatedFlights);
+    }
+}
